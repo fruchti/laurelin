b5b2e62b1ae25733bec36dc7dfa8788a45e4d511
fruchti <fruchti@gvfr.de>
2020-07-16 18:43:40 +0200

    Add order array to help with reassignment

---
+++ src/animation.c
@@ -0,0 +1,18 @@
+#include "animation.h"
+#include "led.h"
+
+const unsigned int Animation_LEDOrder[LED_COUNT] =
+{
+    // Red
+     0,  1,  2,  3,  4,  5,
+    // Green
+     6,  7,  8,  9, 10, 11,
+    // Blue
+    12, 13, 14, 15, 16, 17,
+    // Yellow
+    18, 19, 20, 21, 22, 23,
+    // Cyan
+    24, 25, 26, 27, 28, 29,
+    // Fuchsia
+    30, 31
+};
